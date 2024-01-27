f0d50c123b56a542c71e9732fa8354fa71b57665
DmitriyKaplan28 <dsk007@mail.ru>
2024-01-28 01:15:35 +0300

    feat: add card component

---
+++ src/components/ui/card/Card.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Card } from '.'
+import { CSSProperties } from 'react'
+import { Typography } from '@/components/ui/typography'
+
+const meta = {
+    title: 'Components/Card',
+    component: Card,
+    tags: ['autodocs'],
+} satisfies Meta<typeof Card>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const contentStyles: CSSProperties = { textAlign: 'center', fontSize: '24px' }
+
+
+export const Default: Story = {
+    args: {
+        //TODO fix the mistake
+        //children: <Typography>Card</Typography>,
+        style: {
+            width: '300px',
+            height: '300px',
+            padding: '24px',
+        },
+    },
+}
---
+++ src/components/ui/card/Card.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+import s from './Card.module.scss'
+import cn from 'classnames'
+
+export type CardProps = {} & ComponentPropsWithoutRef<'div'>
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(({ className, ...restProps }, ref) => {
+  const classNames = {
+    root: cn(s.root, className),
+  }
+
+  return <div ref={ref} className={classNames.root} {...restProps}></div>
+})
